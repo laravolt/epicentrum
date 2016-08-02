ec23fb9e5f8afc7c87178027261998c8fe0ddbaa
Bayu Hendra <bayu.hendra@javan.co.id>
2016-08-02 11:58:50 +0700

    adjust suitable

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
     //->addToolbar('With Selected:')
     //->addToolbar(render('packages.suitable.delete'))
     ->columns([
-        new \Laravolt\Suitable\Components\Checkall(),
+        new \Laravolt\Suitable\Columns\Checkall(),
         ['header' => trans('epicentrum::users.name'), 'raw' => function($data){
             return "<img class='ui image avatar' src='" . $data->present('avatar') . "'>" . " " . $data->present('name');
         }],
