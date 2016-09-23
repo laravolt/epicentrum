3e4ff0a4b02658999f03fee9b8668c9154bef5f1
Bayu Hendra <bayu.hendra@javan.co.id>
2016-09-23 16:08:32 +0700

    styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,16 +10,15 @@
     <div class="ui divider hidden"></div>
 
     {!! Suitable::source($users)
-    ->title(trans('epicentrum::users.pages.index.header'))
     ->columns([
-        new \Laravolt\Suitable\Columns\Checkall(),
+        //new \Laravolt\Suitable\Columns\Checkall(),
         ['header' => trans('epicentrum::users.name'), 'raw' => function($data){
             return "<img class='ui image avatar' src='" . Laravolt\Avatar\Facade::create($data->name)->toBase64() . "'>" . " " . $data->name;
         }],
         ['header' => trans('epicentrum::users.email'), 'field' => 'email'],
         ['header' => trans('epicentrum::users.status'), 'field' => 'status'],
         ['header' => false, 'raw' => function($data){
-            return "<a href='".route('epicentrum::users.edit', $data->id)."'><i class='icon ellipsis setting'></i></a>";
+            return "<a class='ui button mini' href='".route('epicentrum::users.edit', $data->id)."'>".trans('epicentrum::action.edit')."</a>";
         }],
     ])
     ->render() !!}
