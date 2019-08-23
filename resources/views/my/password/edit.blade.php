50bcfd013635f68e9cc846d1cafbdff155c2614d
Bayu Hendra Winata <uyab.exe@gmail.com>
2019-08-23 11:07:10 +0700

    styling edit my password

--- resources/views/my/password/edit.blade.php
+++ resources/views/my/password/edit.blade.php
@@ -2,25 +2,16 @@
 
 @section('content')
 
-    <h2 class="ui header">Edit Password</h2>
-    {!! SemanticForm::open()->action(route('epicentrum::my.password.update')) !!}
-
-    <div class="field">
-        <label>@lang('epicentrum::users.password_current')</label>
-        {!! SemanticForm::password('password_current') !!}
+    <div class="ui segments">
+        <div class="ui segment"><h3>@lang('Edit Password')</h3></div>
+        <div class="ui segment">
+            {!! form()->open()->action(route('epicentrum::my.password.update'))->horizontal() !!}
+            {!! form()->password('password_current')->label(__('epicentrum::users.password_current')) !!}
+            {!! form()->password('password')->label(__('epicentrum::users.password_new')) !!}
+            {!! form()->password('password_confirmation')->label(__('epicentrum::users.password_new_confirmation')) !!}
+            {!! form()->action(form()->submit(__('epicentrum::action.save'))) !!}
+            {!! form()->close() !!}
+        </div>
     </div>
-    <div class="field">
-        <label>@lang('epicentrum::users.password_new')</label>
-        {!! SemanticForm::password('password') !!}
-    </div>
-    <div class="field">
-        <label>@lang('epicentrum::users.password_new_confirmation')</label>
-        {!! SemanticForm::password('password_confirmation') !!}
-    </div>
-    <div class="ui divider hidden"></div>
-
-    <button class="ui button primary" type="submit" name="submit" value="1">@lang('epicentrum::action.save')</button>
-    </div>
-    {!! SemanticForm::close() !!}
 
 @endsection
